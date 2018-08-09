452fe4b5c2161b968fcee5bd00592d7a51d69550
root <jeanandrewfuentes@gmail.com>
2018-08-09 10:30:21 +0800

    Fix threading bug in tcp_server

--- 2-network/tcp_server.py
+++ 2-network/tcp_server.py
@@ -31,7 +31,7 @@ while True:
     print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
 
     # spin up our client thread to handle incoming data
-    client_handler = threading.Thread(target=handle_client,args=(client))
+    client_handler = threading.Thread(target=handle_client,args=(client,))
     client_handler.start()
 
 
