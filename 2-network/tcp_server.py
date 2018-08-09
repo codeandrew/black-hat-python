d5a993a899b752cbeb804dc5eac9d35dbdde0b4f
root <root@localhost.localdomain>
2018-08-09 09:24:41 +0800

    Move network py to network folder

--- 2-network/tcp_server.py
+++ 2-network/tcp_server.py
@@ -26,9 +26,8 @@ def handle_client(client_socket):
     client_socket.close()
 
 while True:
-
     client, addr = server.accept()
-
+    
     print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
 
     # spin up our client thread to handle incoming data
