cadedc48f4cd3408b189e40dd895fa5751c39e3a
root <jeanandrewfuentes@gmail.com>
2018-08-10 10:54:00 +0800

    Add server loop

--- 2-network/bhpnet.py
+++ 2-network/bhpnet.py
@@ -63,12 +63,46 @@ def client_sender(buffer):
                 # send it off 
                 client.send(buffer)
 
-        except:
+    except:
 
-            print "[*] Exception! Exiting."
+        print "[*] Exception! Exiting."
 
-            # tear down the connection
-            client.close()
+        # tear down the connection
+        client.close()
+
+def server_loop():
+    global target
+
+    # if no target is defined, we listen on all interfaces
+    if not len(target):
+        target = "0.0.0.0"
+
+        server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        server.bind((target,port))
+
+        server.listen(5)
+
+        while True:
+            client_socket, addr = server.accept()
+
+            # spin off a thread to handle our new client 
+            client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+            client_thread.start()
+
+def run_command(command):
+
+    # trim new line
+    command = command.rstrip()
+
+    # run the command and get the output back
+    try:
+        output = subprocess.check_output(command,stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = "Failed to execute command.\r\n"
+
+    # send the output back to the client
+    return output
+
 
 def main():
     global listen
@@ -122,7 +156,7 @@ def main():
     # depending on our command line options above
 
     if listen:
-        server_loop
+        server_loop()
 
 main()
 
