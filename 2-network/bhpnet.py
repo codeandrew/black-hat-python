bc4b619cf73968ea2e693d92818c6a76665993c5
root <jeanandrewfuentes@gmail.com>
2018-08-10 11:58:12 +0800

    Create client_handler method in bhpnet

--- 2-network/bhpnet.py
+++ 2-network/bhpnet.py
@@ -31,6 +31,7 @@ def usage():
     
     sys.exit(0)
 
+
 def client_sender(buffer):
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -70,6 +71,7 @@ def client_sender(buffer):
         # tear down the connection
         client.close()
 
+
 def server_loop():
     global target
 
@@ -89,6 +91,7 @@ def server_loop():
             client_thread = threading.Thread(target=client_handler, args=(client_socket,))
             client_thread.start()
 
+
 def run_command(command):
 
     # trim new line
@@ -102,6 +105,66 @@ def run_command(command):
 
     # send the output back to the client
     return output
+
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+
+    # check for upload
+    if len(upload_destination):
+
+        # read in all of the bytes and write to our destination
+        file_buffer = ""
+        
+        # keep reading the data until none is available
+        while True:
+            data = client.socket.recv(1024)
+
+            if not data:
+                break
+            else:
+                file_buffer += data
+
+        # now we take these bytes and try to write them out
+        try: 
+            file_descriptor = open(upload_destination, "wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+
+            # acknowledgement that we wrote the file out
+            client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
+
+        except: 
+            client_socket.send("Failed to save file in %s\r\n" % upload_destination)
+
+    # check for command execution
+    if len(execute):
+
+        # run the command
+        output = run_command(execute)
+
+        client_socket.send(output)
+
+    # now we go into another loop if a command shell was requested
+    if command:
+
+        while True:
+            # show simple prompt
+            client_socket.send("<BHP:#> ")
+
+                # now we receive until we see a linefeed ( enter key )
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+
+
+            # send back the command output
+            response = run_command(cmd_buffer)
+
+            # send back the response
+            client_socket.send(response)
 
 
 def main():
