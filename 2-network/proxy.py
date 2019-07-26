bcd0e604150c012c3efa269c438a80ac16b1f3ff
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-07-26 16:27:02 +0800

    Update to python3 format

--- 2-network/proxy.py
+++ 2-network/proxy.py
@@ -11,11 +11,11 @@ def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
     try:
         server.bind((local_host,local_port))
     except:
-        print "[!!] Failed to listen on %s:%d" % (local_host, local_port)
-        print "[!!] Check for other listening sockets or correct permissions."
+        print('[!!] Failed to listen on %s:%s' % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions.")
         sys.exit(0)
 
-    print "[*] Listening on %s:%d % (local_host,local_port)
+    print("[*] Listening on %s:%d" % (local_host, local_port))
 
     server.listen(5)
 
@@ -23,7 +23,7 @@ def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
         client_socket, addr = server.accept()
 
         # print out the local connection information
-        print "[==>] Received incomming connection from %s:%d % (addr[0],addr[1]) "
+        print("[==>] Received incomming connection from %s:%d" % (addr[0], addr[1]) )
 
         # start a thread to talk to the remote host
         proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket,remote_host,remote_port,receive_first))
@@ -47,7 +47,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
 
         # if we have data to send to our local client, send it
         if len(remote_buffer):
-            print "[<==] Sending %d bytes to localhost." %len(remote_buffer)
+            print("[<==] Sending %d bytes to localhost." % len(remote_buffer))
             client_socket.send(remote_buffer)
 
     # now lets loop read from local,
@@ -58,7 +58,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
         local_buffer = receive_from(client_socket)
 
         if len(local_buffer):
-            print "[==>] Received %d bytes from localhost." %  len(local_buffer)
+            print("[==>] Received %d bytes from localhost." % len(local_buffer))
             hexdump(local_buffer)
 
             # send it to our request handler
@@ -66,13 +66,13 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
 
             # send off the data to the remote host
             remote_socket.send(local_buffer)
-            print "[==>] Sent to remote."
+            print("[==>] Sent to remote.")
 
             # receive back the response
             remote_buffer = receive_from(remote_socket)
 
             if len(remote_buffer):
-                print "[<==] Received %d bytes from remote ." % len(remote_buffer)
+                print("[<==] Received %d bytes from remote ." % len(remote_buffer))
                 hexdump(remote_buffer)
 
                 # send to our response handler
@@ -81,13 +81,13 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
                 # send the response to the local socket
                 client_socket.send(remote_buffer)
 
-                print "[<==] Sent to localhost."
+                print("[<==] Sent to localhost.")
 
             # if no more data on either side, close the connections
             if not len(local_buffer) or not len(remote_buffer):
                 client_socket.close()
                 remote_socket.close()
-                print "[*] No more data. Closing connections."
+                print("[*] No more data. Closing connections.")
 
                 break
 
@@ -95,8 +95,8 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
 def main():
     # no fancy command line parsing here
     if len(sys.argv[1:]) != 5:
-        print "Usage: ./proxy.py [localhost] [localport] [remotehost] [remoteport] [receivefirst]"
-        print "Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True"
+        print("Usage: ./proxy.py [localhost] [localport] [remotehost] [remoteport] [receivefirst]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
 
     # setup local listening parameters
     local_host = sys.argv[1]
