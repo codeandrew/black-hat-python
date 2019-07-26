f1cf311a58544289bd28637e277ee53b269c59a0
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-07-26 17:07:07 +0800

    create sshcmd for windows

---
+++ 2-network/bh_sshcmd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import threading
+import paramiko
+import subprocess
+
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    # client.load_host_keys('$HOME/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(1024))
+    return
+
+ssh_command('13.250.23.160', 'whitecloak', 'P@ssw0rdWC', "id")
